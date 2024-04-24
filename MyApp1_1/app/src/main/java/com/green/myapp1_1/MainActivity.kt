c909123935271ccea9ac9ea9dbad40cc0e8e0073
hayaboy <hayaboy@korea.ac.kr>
2024-04-24 12:27:18 +0900

    4_24_로그인화면_계산기1

--- MyApp1_1/app/src/main/java/com/green/myapp1_1/MainActivity.kt
+++ MyApp1_1/app/src/main/java/com/green/myapp1_1/MainActivity.kt
@@ -1,17 +1,84 @@
 package com.green.myapp1_1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.lang.Double
+import kotlin.Any
+import kotlin.String
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var edit1: EditText;   lateinit var edit2 : EditText
+    lateinit var btnAdd : Button;   lateinit var btnSub : Button
+    lateinit var btnMul : Button;   lateinit var btnDiv : Button
+    lateinit var textResult : TextView
+    lateinit var num1:String;    lateinit var num2 : String
+    var result:Any?=null
+
+    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main3)
+
+        edit1 = findViewById<EditText>(R.id.Edit1)
+        edit2 = findViewById<EditText>(R.id.Edit2)
+        btnAdd = findViewById<Button>(R.id.BtnAdd)
+        btnSub = findViewById<Button>(R.id.BtnSub)
+        btnMul = findViewById<Button>(R.id.BtnMul)
+        btnDiv = findViewById<Button>(R.id.BtnDiv)
+        textResult = findViewById<TextView>(R.id.TextResult)
+
+        btnAdd.setOnTouchListener { view, motionEvent ->
+            num1= edit1.text.toString()
+            num2= edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+
+
+            textResult.text="Result는 " + result.toString()
+            false
+        }
+
+        btnSub.setOnTouchListener { view, motionEvent ->
+            num1= edit1.text.toString()
+            num2= edit2.text.toString()
+            result = Integer.parseInt(num1) - Integer.parseInt(num2)
+
+            textResult.text="Result는 " + result.toString()
+            false
+        }
+
+        btnMul.setOnTouchListener { view, motionEvent ->
+            num1= edit1.text.toString()
+            num2= edit2.text.toString()
+            result = Integer.parseInt(num1) * Integer.parseInt(num2)
+
+            textResult.text="Result는 " + result.toString()
+            false
+        }
+
+
+        btnDiv.setOnTouchListener { view, motionEvent ->
+            num1= edit1.text.toString()
+            num2= edit2.text.toString()
+            Log.i("multiply", num1)
+            Log.i("multiply", num2)
+            result = Double.parseDouble(num1) / Double.parseDouble(num2)
+
+//            String.format("%.2f", result)
+
+            textResult.text="Result는 " + String.format("%.2f", result)
+            false
+        }
 
     }
 }
